c2f7b4e48b7233728538d0641a1497ebf2f9f617
thesayyn <thesayyn@gmail.com>
2023-02-26 18:04:15 +0300

    feat: implement publishing

--- cmd/build.go
+++ cmd/build.go
@@ -1,89 +1,127 @@
 package cmd
 
 import (
+	"fmt"
 	"path/filepath"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/layout"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
 
+	cp "github.com/otiai10/copy"
 	"github.com/spf13/cobra"
 	"github.com/thesayyn/no/pkg/build"
 	"github.com/thesayyn/no/pkg/cache"
 	"github.com/thesayyn/no/pkg/node"
 )
 
+func saveTarball(p string, out string, tag string) error {
+	l, err := layout.ImageIndexFromPath(p)
+	if err != nil {
+		return err
+	}
+	ref, err := name.ParseReference(tag)
+	if err != nil {
+		return err
+	}
+
+	mn, err := l.IndexManifest()
+	if err != nil {
+		return err
+	}
+	img, err := l.Image(mn.Manifests[0].Digest)
+	if err != nil {
+		return err
+	}
+	if err = tarball.WriteToFile(p, ref, img); err != nil {
+		return err
+	}
+	return nil
+}
+
 func NewRun() *cobra.Command {
 
-	var output string
+	var workingDir string
+	var format string
 
 	cmd := &cobra.Command{
-		Use:   "build path...",
-		Short: "Build and publish container images from the given sub-paths.",
+		Use:   "build path output",
+		Args:  cobra.ExactValidArgs(2),
+		Short: "Build and publish container images from the given sub-path.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			if len(args) == 0 {
-				args = []string{"."}
+			project := args[0]
+			output := args[1]
+
+			currentWorkingDir := project
+			if workingDir != "" {
+				currentWorkingDir = workingDir
 			}
 
 			dc := cache.NewDiskCache()
-
 			if err := dc.Setup(); err != nil {
 				return err
 			}
 
-			for _, v := range args {
-				options := build.NewOptions(DEFAULT_MTIME, v, v, DEFAULT_BASE, *DEFAULT_PLATFORM)
-				lock, err := node.FindPackageLock(v)
+			options := build.NewOptions(DEFAULT_MTIME, project, currentWorkingDir, DEFAULT_BASE, *DEFAULT_PLATFORM)
+			lock, err := node.FindPackageLock(project)
+			if err != nil {
+				return err
+			}
+			package_json := filepath.Join(project, "package.json")
+
+			appTask := build.NewAppTask(options, project)
+			nodeModulesTask := build.NewNodeModulesTask(options, project, *lock)
+
+			appOutput, err := cache.RunIfNotCached(dc, appTask)
+			if err != nil {
+				return err
+			}
+			nodeModulesOutput, err := cache.RunIfNotCached(dc, nodeModulesTask)
+			if err != nil {
+				return err
+			}
+
+			imageTask := build.NewImageTask(options, []build.Output{appOutput, nodeModulesOutput}, package_json)
+			imageOutput, err := cache.RunIfNotCached(dc, imageTask)
+			if err != nil {
+				return err
+			}
+
+			if format == "tarball" {
+				saveTarball(imageOutput.Path(), output, fmt.Sprintf("no/%s:latest", filepath.Clean(project)))
+			} else if format == "oci" {
+				cp.Copy(imageOutput.Path(), output)
+			} else {
+				ref, err := name.ParseReference(output)
 				if err != nil {
 					return err
 				}
-				package_json := filepath.Join(v, "package.json")
-
-				appTask := build.NewAppTask(options, v)
-				nodeModulesTask := build.NewNodeModulesTask(options, v, *lock)
-
-				appOutput, err := cache.RunIfNotCached(dc, appTask)
+				l, err := layout.ImageIndexFromPath(imageOutput.Path())
 				if err != nil {
 					return err
 				}
-				nodeModulesOutput, err := cache.RunIfNotCached(dc, nodeModulesTask)
+				mn, err := l.IndexManifest()
 				if err != nil {
 					return err
 				}
-
-				imageTask := build.NewImageTask(options, []build.Output{appOutput, nodeModulesOutput}, package_json)
-				imageOutput, err := cache.RunIfNotCached(dc, imageTask)
+				img, err := l.Image(mn.Manifests[0].Digest)
 				if err != nil {
 					return err
 				}
-
-				spew.Dump(appOutput, nodeModulesOutput, imageOutput)
-
-				// if output != "" {
-				// 	p, err := layout.FromPath(output)
-				// 	if err != nil {
-				// 		p, err = layout.Write(output, empty.Index)
-				// 		if err != nil {
-				// 			return err
-				// 		}
-				// 	}
-
-				// 	p.AppendImage(img)
-				// } else {
-				// 	ref, err := name.ParseReference(fmt.Sprintf("no/%s:latest", filepath.Clean(v)))
-				// 	if err != nil {
-				// 		return err
-				// 	}
-				// 	if err = tarball.WriteToFile(fmt.Sprintf("%s.tar", v), ref, img); err != nil {
-				// 		return err
-				// 	}
-				// }
+				err = remote.Write(ref, img, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+				if err != nil {
+					return err
+				}
 			}
 
 			return nil
 		},
 	}
 
-	cmd.Flags().StringVarP(&output, "output", "o", "", "")
-
+	cmd.Flags().StringVarP(&format, "format", "", "", "")
+	cmd.Flags().StringVarP(&workingDir, "working-dir", "", "", "")
 	return cmd
 }
--- go.mod
+++ go.mod
@@ -4,23 +4,19 @@ go 1.19
 
 require (
 	github.com/bmatcuk/doublestar/v4 v4.2.0
-	github.com/davecgh/go-spew v1.1.1
+	github.com/fatih/color v1.14.1
 	github.com/google/go-containerregistry v0.11.0
+	github.com/otiai10/copy v1.2.0
 	github.com/spf13/cobra v1.5.0
 	github.com/wealdtech/go-merkletree v1.0.0
 )
 
 require (
-	github.com/Microsoft/go-winio v0.5.2 // indirect
 	github.com/containerd/stargz-snapshotter/estargz v0.12.0 // indirect
 	github.com/docker/cli v20.10.17+incompatible // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/docker v20.10.17+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.6.4 // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
-	github.com/fatih/color v1.14.1 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/klauspost/compress v1.15.8 // indirect
@@ -35,7 +31,6 @@ require (
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/vbatts/tar-split v0.11.2 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
 	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
